288f0883509dc800cfb93cad16bc32071d61ec79
Raul Romero <rromero3@gmail.com>
2015-12-21 03:41:01 +0000

    initial commit

---
+++ bin/odoo.py
@@ -0,0 +1,4 @@
+#!/opt/bitnami/python/bin/python
+# EASY-INSTALL-SCRIPT: 'odoo==9.0.post20151109','odoo.py'
+__requires__ = 'odoo==9.0.post20151109'
+__import__('pkg_resources').run_script('odoo==9.0.post20151109', 'odoo.py')
